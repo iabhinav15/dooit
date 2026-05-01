a377cff8cbad511199d86771eaf4654f7081b71a
Abhinav Singh <official.abhinavsingh15@gmail.com>
2026-05-02 00:43:43 +0530

    new release version 1.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.dooit.app',
-  appName: 'dooit',
-  webDir: 'out',
+  appId: "com.dooit.app",
+  appName: "Dooit",
+  webDir: "out",
   server: {
-    url: 'http://192.168.1.37:3000', // For local dev testing. Change to your Vercel URL in production
-    cleartext: true
-  }
+    url: "https://dooitapp.netlify.app",
+    cleartext: true,
+  },
 };
 
 export default config;
